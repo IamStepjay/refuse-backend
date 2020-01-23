002b1fc7d4a6051ff488950f8b7fb28f1e4ba176
Stephen Ojerinde <ostephenayomide@gmail.com>
2020-01-23 08:25:44 +0100

    ready for heroku deployment

--- src/main/java/com/example/refuse/model/User.java
+++ src/main/java/com/example/refuse/model/User.java
@@ -1,6 +1,7 @@
 package com.example.refuse.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "user")
 public class User {
 
@@ -18,9 +20,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
     @NotNull(message = "Phone Number is required")
-    @Column(name = "phoneNumber")
+    @Column(name = "phone_number")
     private String phoneNumber;
-    @Column(name = "fullName")
+    @Column(name = "full_name")
     private String fullName;
     @NotNull(message = "Password is required")
     @Column(name = "password")
--- src/main/java/com/example/refuse/repository/UserRepository.java
+++ src/main/java/com/example/refuse/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.refuse.repository;
 
 import java.util.Optional;
+import javax.persistence.*;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.refuse.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 	
 	public Optional<User> findByPhoneNumber(String phoneNumber);
